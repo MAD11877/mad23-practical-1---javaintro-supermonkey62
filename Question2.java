0ffa973d6f848266959d22f132ba8639fe3fff6a
Kenneth <kennethbwwu@gmail.com>
2023-04-19 15:02:00 +0800

    Question 2

--- Question2.java
+++ Question2.java
@@ -11,6 +11,15 @@ public class Question2
      */
      
     Scanner in = new Scanner(System.in);
+
+    System.out.print("Enter height in meters: ");
     double height = in.nextDouble();
+
+    System.out.print("Enter weight in kg: ");
+    double weight = in.nextDouble();
+
+    double bmi = weight / (height * height);
+    System.out.println("Your BMI is " + bmi);
+
   }
 }
